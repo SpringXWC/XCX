1d9c48d9a64c10ca39ddb6605f9bd82be0bb8fb5
SpringXWC <1367232281@qq.com>
2018-04-13 17:54:48 +0800

    更新一下

--- pages/order/order.js
+++ pages/order/order.js
@@ -1,7 +1,18 @@
+
+const app = getApp();
+
 Page({
   data:{
-
-  }
-  
+    list:[
+      { imgUrl: '../../images/order/one.jpg', url: '../index/index', text:'这个故事很长很长很长很长。。。'},
+      { imgUrl: '../../images/order/two.jpg', url: '../welcome/welcome', text: '这个故事更长更长更长更长更长。。' },
+      { imgUrl: '../../images/order/three.jpg', url: '../personal/personal', text: '这个故事有意思意思意思。。。' },
+      { imgUrl: '../../images/order/one.jpg', url: '../service/service', text: '好了，我没有词了。。。' },
+      { imgUrl: '../../images/order/two.jpg', url: '../personal/personal', text: '再说一句吧。。。' },
+      { imgUrl: '../../images/order/three.jpg', url: '../index/index', text: '好像还想再说下去。。。' },
+      { imgUrl: '../../images/order/two.jpg', url: '../welcome/welcome', text: '嗯，这个是最后一句啦。。' },
+      { imgUrl: '../../images/order/one.jpg', url: '../service/service', text: '哈哈哈，这个才是最后一句。。。。' },
+    ]
+  } 
 })
  
